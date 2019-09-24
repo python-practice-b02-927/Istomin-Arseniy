8e50df473c003377b73ce26d4035903b9949dc51
IstominArseniy <istomin15arseniy@gmail.com>
2019-09-24 15:12:37 +0300

    add gravitation

--- lab3_2/task_1.py
+++ lab3_2/task_1.py
@@ -1,32 +1,36 @@
 import graphics as gr
 
 window = gr.GraphWin("Model", 600, 600)
-g = 10
-
-def main(window):
+g = 5000
+time = 0.01
+def main():
     coords0 = gr.Point(50, 100)
-    velocity0 = gr.Point(10, 5)
-    move_circle(coords0, velocity0, window)
+    velocity0 = gr.Point(5, 6)
+    move_circle(coords0, velocity0)
     window.getMouse()
     window.close()
 
 
-def move_circle(coords0, velocity0, window):
+def move_circle(coords0, velocity0):
     circle = init_circle(coords0, 10, 'blue')
     velocity = velocity0
     circle.draw(window)
-    for i in range(1000):
+    for i in range(2000):
         circle.move(velocity.x, velocity.y)
-        gr.time.sleep(0.01)
+        gr.time.sleep(time)
         velocity = change_velocity(circle, velocity)
 
 
 def change_velocity(circle, velocity):
     centre = circle.getCenter()
-    if centre.x > 600 or centre.x < 0:
+    radius = circle.getRadius()
+    if centre.x > 600-radius or centre.x < 0+radius:
         velocity.x = -velocity.x
-    if centre.y < 0 or centre.y > 600:
+    if centre.y < 0+radius or centre.y > 600-radius:
         velocity.y = - velocity.y
+    velocity.y = velocity.y + g * time**2/2
+    #if abs(velocity.y)<
+    #print(velocity.y)
     return velocity
 
 
@@ -37,4 +41,4 @@ def init_circle(coords0, size, color):
     return circle
 
 
-main(window)
+main()
