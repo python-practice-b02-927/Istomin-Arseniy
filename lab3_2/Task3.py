bbe277c618c5544d2f91ac87175de1474003a327
IstominArseniy <istomin15arseniy@gmail.com>
2019-09-28 23:10:28 +0300

    request_for_constant_parameters(rename function) is works

--- lab3_2/Task3.py
+++ lab3_2/Task3.py
@@ -1,9 +1,14 @@
 def main():
-    request_for_constant_parameter()
+    request_for_constant_parameters()
 
 
-def request_for_constant_parameter():
-    pass
+def request_for_constant_parameters():
+    print("Enter potential well depth")
+    potential_well_depth = int(input())
+    print("Enter zero dist")
+    zero_dist = int(input())
+    print("Enter particles amount")
+    particles_amount = int(input())
 
 
 main()
