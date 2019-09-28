3accfb50dafc28808f11db804f6309663cd64958
IstominArseniy <istomin15arseniy@gmail.com>
2019-09-28 23:39:13 +0300

    add particle_init function and create cycle to init particles

--- lab3_2/Task3.py
+++ lab3_2/Task3.py
@@ -1,14 +1,22 @@
-#global variables
+# global variables
 potential_well_depth = 0
 zero_dist = 0
 particles_amount = 0
 
+
 def main():
     request_for_constant_parameters()
+    particles = []
+    for i in range(zero_dist):
+        particle = particle_init()
+        particles.append(particle)
 
 
+def particle_init():
+    pass
 
 def request_for_constant_parameters():
+    global particles_amount, potential_well_depth, zero_dist
     print("Enter potential well depth")
     potential_well_depth = int(input())
     print("Enter zero dist")
